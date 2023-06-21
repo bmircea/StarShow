863625de693f7162d5df8339385e12ae6ba1cd2d
adnwhn <dianadoitrei@gmail.com>
2023-06-22 01:55:43 +0300

    object animator and motion layout animations

--- app/src/main/java/com/example/starshow/MainActivity.kt
+++ app/src/main/java/com/example/starshow/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.starshow
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import com.example.starshow.databinding.ActivityMainBinding
@@ -12,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var toggle: ActionBarDrawerToggle // open/close menu
 
+    //lateinit var animateButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
@@ -32,7 +35,34 @@ class MainActivity : AppCompatActivity() {
                 true
             }
         }
-    }
+
+        //object animator
+        //animateButton = findViewById(R.id.animateButton)
+        //animateButton.setOnClickListener {
+            //val tY = ObjectAnimator.ofFloat(R.id.redView, View.TRANSLATION_Y, R.id.redView.translationY, R.id.redView.translationY + 100f)
+        //}
+
+        binding.imageStar.setOnClickListener {
+            binding.imageStar.animate().apply {
+                duration = 1000
+                alpha(.5f)
+                scaleXBy(.5f)
+                scaleYBy(.5f)
+                rotationYBy(360f) // continue adding 360 degrees to the animation
+                translationYBy(200f)
+            }.withEndAction {
+                binding.imageStar.animate().apply {
+                    duration = 1000
+                    alpha(1f)
+                    scaleXBy(-.5f)
+                    scaleYBy(-.5f)
+                    rotationYBy(360f) // continue adding 360 degrees to the animation
+                    translationYBy(-200f)
+                }.start()
+            }
+        }
+
+   }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (toggle.onOptionsItemSelected(item)){
